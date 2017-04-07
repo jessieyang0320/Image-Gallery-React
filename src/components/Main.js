ac4bab8c95290e08ce7200486b5ddd9075e2f035
Jessie Yang <jessieyang@Jessies-MacBook-Air.local>
2017-04-07 12:02:27 -0400

    resized all imagesfor a better looking, images better to be squared, correct class to className in component

--- src/components/Main.js
+++ src/components/Main.js
@@ -24,10 +24,10 @@ function genImageURL(imageDataArr) {
 class ImgFigure extends React.Component {
 	render (){
 		return(
-			<figure> 
+			<figure className ="img-figure"> 
 			  <img src= {this.props.data.imageURL} alt={this.props.data.title}/>
 			  <figcaption> 
-			    <h2>{this.props.data.title}</h2>
+			    <h2 className = "img-title">{this.props.data.title}</h2>
 			  </figcaption>
 			</figure> 
 			)
